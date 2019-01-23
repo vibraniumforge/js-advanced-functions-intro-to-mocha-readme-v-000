2624100be35a509083af073cb162bf5013b01a27
Greg Morgan <gregm9999@gmail.com>
2019-01-23 21:43:09 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth() {
+  return true;
+}
+
+function sayHey() {
+  return "Hey!"
+}
+
+function sayHeyFriend(name) {
+  return `Hey, ${name}!`;
+}
+
+function favoriteIceCream(iceCream) {
+  return `I love ${iceCream}`;
+}
+
+function shouting(str) {
+  return str.toUpperCase();
+}
+
+function roundDown(num) {
+  return Math.floor(num);
+}
